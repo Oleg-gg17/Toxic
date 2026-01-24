514c51e7ca47c5159fa5edff331292c6cbab492f
Oleg Glebov <oleg.glebov.com@icloud.com>
2026-01-24 21:49:02 +0300

    Fix Task10

--- src/homeworkLevel1/Task10.java
+++ src/homeworkLevel1/Task10.java
@@ -1,20 +1,20 @@
 package homeworkJava_1;
+
 public class Task10 {
 
-/**
- * Сумма чисел до N
- * Написать программу, которая принимает число N и с помощью цикла считает сумму всех чисел от 1 до N.
- */
+    /**
+     * Сумма чисел до N
+     * Написать программу, которая принимает число N и с помощью цикла считает сумму всех чисел от 1 до N.
+     */
 
     public static void main(String[] args) {
-        int N = 7;
-        int S = SumArray(N);
-        System.out.println("Сумма всех чисел от 1 до " + N + " равна: " + S);
+        int mean = 7;
+        int result = sumArray(mean);
+        System.out.println("Сумма всех чисел от 1 до " + mean + " равна: " + result);
     }
 
-    public static int SumArray(int N) {
-        int Summa = (N * (N + 1)) / 2;
-        return Summa;
+    public static int sumArray(int mean) {
+        int summa = (mean * (mean + 1)) / 2;
+        return summa;
     }
 }
-
