c43435b12db7b335ad7afd729e369dfc62a779c8
Oleg Glebov <oleg.glebov.com@icloud.com>
2026-01-31 13:36:18 +0300

    CS3 Practical task

---
+++ homework/src/com/cs/level1/task10/task10Runner.java
@@ -0,0 +1,20 @@
+package com.cs.level1.task10;
+
+/**
+ * Сумма чисел до N
+ * Написать программу, которая принимает число N и с помощью цикла считает сумму всех чисел от 1 до N.
+ */
+
+public class task10Runner {
+
+    public static void main(String[] args) {
+        int mean = 7;
+        int result = sumArray(mean);
+        System.out.println("Сумма всех чисел от 1 до " + mean + " равна: " + result);
+    }
+
+    public static int sumArray(int mean) {
+        int summa = (mean * (mean + 1)) / 2;
+        return summa;
+    }
+}
